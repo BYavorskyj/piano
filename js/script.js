c8a5ae679fbe3d062a947223c90db568b80c158b
BYavorskyi <boyavor@gmail.com>
2019-07-07 22:15:55 +0300

    d

--- js/script.js
+++ js/script.js
@@ -1,24 +1,11 @@
-// function hendlePlay(e){
-//     let audio = document.getElementById("myAudio");
-//     audio.hendlePlay(e);
-//               }
 
-// let audio = new Audio(e);
-
-// audio.play();
-// const btns = document.getElementsByClassName('button');
-// for(let i=0; i>=btns.length; i++) {
-//     btns[i].addEventListener('click', hendlePlay)
-// }
-
-
-handlePlay = (e) => {
+function handlePlay (e) {
     let audio = new Audio('piano-notes/' + e.target.dataset.n + '.mp3');
     audio.play();
 }
 
 const btns = document.getElementsByClassName('button');
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', hendlePlay
+    btns[i].addEventListener('click', handlePlay
     )
 }
