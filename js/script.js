5d56870f668998927c32b1db3909cf1c38b82cbc
BYavorskyi <boyavor@gmail.com>
2019-07-07 19:47:36 +0300

    d

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 
-// let note = document.querySelectorAll('notes');
-
-// element.onClick = function (event) {
-
-// }
-
-// note.addEventListener('notes',function (event) {
-//     const audio = document.querySelector('notes')
-// } )
-
 function play(){
     let audio = document.getElementsByClassName("audio");
     audio.play();
