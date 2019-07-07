3bbd1d88c7d91818d9b4c581401173fe6d96ffba
BYavorskyi <boyavor@gmail.com>
2019-07-07 21:24:24 +0300

    d

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-let audio = new Audio('piano-notes/1.mp3');
-audio.play();
+function hendleЗlay(){
+    var audio = document.getElementById("myAudio");
+    audio.hendleЗlay();
+              }
 
+let audio = new Audio(e);
+console.log('e.target');
+
+audio.play();
 const btns = document.getElementsByClassName('button');
 for(let i=0; i>=btns.length; i++) {
     btns[i].addEventListener('click', play)
