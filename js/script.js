10ece88357b80411ccee690c626509bd27856caf
BYavorskyi <boyavor@gmail.com>
2019-07-08 17:49:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -6,6 +6,58 @@ function handlePlay (e) {
 
 const btns = document.getElementsByClassName('button');
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', handlePlay
-    )
+    btns[i].addEventListener('click', handlePlay)
 }
+
+
+
+window.addEventListener("keydown", handlePlay);
+
+
+btns.addEventListener('keydown', (e) => {
+    const keyA =65;
+    const keyS =83;
+    const keyD =68;
+    const keyF =70;
+    const keyG =71;
+    const keyH =72;
+    const keyJ =74;
+    const keyK =75;
+
+    switch (e.keyCode) {
+        case keyA:
+            
+            break;
+        case keyS: 
+
+            break;
+    
+        case keyD:
+            
+            break;
+        case keyF: 
+        
+            break;
+        case keyG:
+            
+            break;
+        case keyH: 
+        
+            break;
+        case keyH:
+            
+            break;
+        case keyJ: 
+        
+            break;
+        case keyK:
+            
+            break;
+    
+        default:
+            break;
+    }
+} )
+
+
+btns.addEventListener('keydown', )
