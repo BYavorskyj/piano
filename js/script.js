bfb18e9fff098bdad2fb9026dd1f1b20c1f3b3d4
BYavorskyi <boyavor@gmail.com>
2019-07-23 19:39:09 +0300

    keydown

--- js/script.js
+++ js/script.js
@@ -10,48 +10,46 @@ for (let i = 0; i < btns.length; i++) {
 }
 
 
-
-window.addEventListener("keydown", handlePlay);
+ window.addEventListener("keydown", handlePlay);
 
 
 window.addEventListener('keydown', (e) => {
-    const keyA =65;
-    const keyS =83;
-    const keyD =68;
-    const keyF =70;
-    const keyG =71;
-    const keyH =72;
-    const keyJ =74;
-    const keyK =75;
+    // const keyA =65;
+    // const keyS =83;
+    // const keyD =68;
+    // const keyF =70;
+    // const keyG =71;
+    // const keyH =72;
+    // const keyJ =74;
+    // const keyK =75;
 
     switch (e.keyCode) {
         case keyA:
-            
+            handlePlay (1);
             break;
         case keyS: 
-
-            break;
-    
+            handlePlay (2);
+            break;    
         case keyD:
-            
+            handlePlay (3);
             break;
         case keyF: 
-        
+            handlePlay (4);
             break;
         case keyG:
-            
+            handlePlay (5);
             break;
         case keyH: 
-        
+            handlePlay (6);
             break;
         case keyH:
-            
+            handlePlay (7);
             break;
         case keyJ: 
-        
+            handlePlay (8);
             break;
         case keyK:
-            
+            handlePlay (9);
             break;
     
         default:
