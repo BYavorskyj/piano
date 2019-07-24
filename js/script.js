2e94769edd86f545f3c314ffcdef440409533eb0
BYavorskyi <boyavor@gmail.com>
2019-07-24 17:46:05 +0300

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function handlePlay (e) {
+function handlePlay(e) {
     let audio = new Audio('piano-notes/' + e.target.dataset.n + '.mp3');
     audio.play();
 }
@@ -10,49 +10,50 @@ for (let i = 0; i < btns.length; i++) {
 }
 
 
- window.addEventListener("keydown", handlePlay);
-
-
+function handlePlay(noteId) {
+    let audio = new Audio('piano-notes/' + noteId + '.mp3');
+    audio.play();
+}
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', () => handlePlay(e.target.dataset.n))
+}
 window.addEventListener('keydown', (e) => {
-    const keyA =65;
-    const keyS =83;
-    const keyD =68;
-    const keyF =70;
-    const keyG =71;
-    const keyH =72;
-    const keyJ =74;
-    const keyK =75;
+    const keyA = 65;
+    const keyS = 83;
+    const keyD = 68;
+    const keyF = 70;
+    const keyG = 71;
+    const keyH = 72;
+    const keyJ = 74;
+    const keyK = 75;
 
     switch (e.keyCode) {
         case keyA:
-            handlePlay (1);
+            handlePlay(1);
             break;
-        case keyS: 
-            handlePlay (2);
-            break;    
+        case keyS:
+            handlePlay(2);
+            break;
         case keyD:
-            handlePlay (3);
+            handlePlay(3);
             break;
-        case keyF: 
-            handlePlay (4);
+        case keyF:
+            handlePlay(4);
             break;
         case keyG:
-            handlePlay (5);
-            break;
-        case keyH: 
-            handlePlay (6);
+            handlePlay(5);
             break;
         case keyH:
-            handlePlay (7);
+            handlePlay(6);
             break;
-        case keyJ: 
-            handlePlay (8);
+        case keyJ:
+            handlePlay(7);
             break;
         case keyK:
-            handlePlay (9);
+            handlePlay(8)
             break;
-    
+
         default:
             break;
     }
-} )
+})
